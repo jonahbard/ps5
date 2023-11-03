5347c2a27fe42cee34aa7c81203a692e3bc77d9d
Jonah Bard <jonahbard@gmail.com>
2023-11-02 20:05:46 -0400

    added method signatures

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
+import java.util.Map;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+
+
+    //don't really need this but could be easier if we use a real graph
+    public AdjacencyMapGraph HMM; // graph that represents the HMM, using Map<String,Map<String, Double>> as nodes and Double as edge labels
+
+    /***
+     * return array of POS that corresponds to the backtraced viterbi most-likely POS list for given string
+     * @param phrase
+     * @return
+     */
+    public String[] viterbiMostLikelyPOSSequence(String phrase){}
+
+    public Map<Map<String, String>, Double> calculateTransitionProbabilities(String POSfile){}
+
+    public Map<String, Map<String, Double>> calculateWordPOSProbabilities(String wordsFile, String POSfile){}
+
+    public void assembleHMM(){} // assemble the HMM from the calculation methods
+
+    public void testHMM(String wordsFile, String POSfile){}
+
     public static void main(String[] args) {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
